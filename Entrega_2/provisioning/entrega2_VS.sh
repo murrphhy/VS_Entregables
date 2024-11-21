785678c1fbc4b17349795ad45e5d5e02eca0f2c9
murrphhy <julio.martin.garci@gmail.com>
2024-11-21 14:53:05 +0100

    Maquina levantada con docker, kind y kubectl

---
+++ Entrega_2/provisioning/entrega2_VS.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Instalación de Docker
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Añadir al usuario 'vagrant' al grupo de Docker para ejecutar comandos sin sudo
+sudo usermod -aG docker vagrant
+
+# Verificar la instalación de Docker
+docker --version
+
+# Actualización de los paquetes que nos vienen de inicio.
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+# Instalación Kind
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+    # Comprobación de la instalación de Kind
+    kind version
+
+# Creación del cluster (kubectl)
+kind create cluster --name cluster-entrega2
+    # Comprobación de la instalación del cluster
+    docker ps
