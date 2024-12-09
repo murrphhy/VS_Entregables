eaf2ddb5596fa619a44b0083595395392af2f07c
murrphhy <julio.martin.garci@gmail.com>
2024-12-09 13:04:13 +0100

    infraestructura levantada

---
+++ Entrega_3/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+
+resource "docker_network" "red_ejercicio" {
+  name = "red_entrega"
+}
+
+resource "docker_volume" "jenkins_data" {
+  name = "jenkins_data"
+}
+
+resource "docker_volume" "jenkins_certs" {
+  name = "jenkins_certs"
+}
+
